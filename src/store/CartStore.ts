06f0cb9400809c19289c4b2110aee8f771f34d00
dromse <dromasko4@gmail.com>
2024-05-03 00:00:31 +0300

    add persistent to cart store

--- src/store/CartStore.ts
+++ src/store/CartStore.ts
@@ -1,5 +1,6 @@
 import { Product } from "@types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type CartItem = {
   productId: string;
@@ -20,88 +21,98 @@ type CartState = {
   decreaseItem: (id: string) => void;
 };
 
-export const useCart = create<CartState>()((set) => ({
-  cart: { cartId: "123", cartItems: [] },
-  cartItems: [],
-  addItem: (product): void =>
-    set((state) => {
-      const cartItemIndex = state.cartItems.findIndex(
-        (obj) => obj.productId === product.id,
-      );
+export const useCart = create<CartState>()(
+  persist(
+    (set) => ({
+      cart: { cartId: "123", cartItems: [] },
+      cartItems: [],
 
-      let amount = 1;
+      addItem: (product): void =>
+        set((state) => {
+          const cartItemIndex = state.cartItems.findIndex(
+            (item) => item.productId === product.id,
+          );
 
-      if (cartItemIndex > -1) {
-        amount = state.cartItems[cartItemIndex].amount + 1;
+          let amount = 1;
 
-        return {
-          cartItems: [
-            ...state.cartItems.filter((item) => item.productId !== product.id),
-            { productId: product.id, amount },
-          ],
-        };
-      }
+          if (cartItemIndex > -1) {
+            amount = state.cartItems[cartItemIndex].amount + 1;
 
-      return {
-        cartItems: [...state.cartItems, { productId: product.id, amount }],
-      };
-    }),
+            return {
+              cartItems: [
+                ...state.cartItems.filter(
+                  (item) => item.productId !== product.id,
+                ),
+                { productId: product.id, amount },
+              ],
+            };
+          }
 
-  deleteItem: (id: string): void =>
-    set((state) => ({
-      cartItems: [...state.cartItems.filter((item) => item.productId !== id)],
-    })),
-
-  increaseItem: (id: string): void =>
-    set((state) => {
-      const cartItemIndex = state.cartItems.findIndex(
-        (obj) => obj.productId === id,
-      );
-
-      let amount = 1;
-
-      if (cartItemIndex > -1) {
-        amount = state.cartItems[cartItemIndex].amount + 1;
-
-        return {
-          cartItems: [
-            ...state.cartItems.filter((item) => item.productId !== id),
-            { productId: id, amount },
-          ],
-        };
-      }
-
-      throw new Error("Not found.");
-    }),
-
-  decreaseItem: (id: string): void => {
-    set((state) => {
-      const cartItemIndex = state.cartItems.findIndex(
-        (obj) => obj.productId === id,
-      );
-
-      let amount = 1;
-
-      if (cartItemIndex > -1) {
-        amount = state.cartItems[cartItemIndex].amount - 1;
-
-        if (amount === 0) {
           return {
-            cartItems: [
-              ...state.cartItems.filter((item) => item.productId !== id),
-            ],
+            cartItems: [...state.cartItems, { productId: product.id, amount }],
           };
-        }
+        }),
 
-        return {
+      deleteItem: (id: string): void =>
+        set((state) => ({
           cartItems: [
             ...state.cartItems.filter((item) => item.productId !== id),
-            { productId: id, amount },
           ],
-        };
-      }
+        })),
 
-      throw new Error("Not found.");
-    });
-  },
-}));
+      increaseItem: (id: string): void =>
+        set((state) => {
+          const cartItemIndex = state.cartItems.findIndex(
+            (item) => item.productId === id,
+          );
+
+          let amount = 1;
+
+          if (cartItemIndex > -1) {
+            amount = state.cartItems[cartItemIndex].amount + 1;
+
+            return {
+              cartItems: [
+                ...state.cartItems.filter((item) => item.productId !== id),
+                { productId: id, amount },
+              ],
+            };
+          }
+
+          throw new Error("Not found.");
+        }),
+
+      decreaseItem: (id: string): void => {
+        set((state) => {
+          const cartItemIndex = state.cartItems.findIndex(
+            (item) => item.productId === id,
+          );
+
+          let amount = 1;
+
+          if (cartItemIndex > -1) {
+            amount = state.cartItems[cartItemIndex].amount - 1;
+
+            if (amount === 0) {
+              return {
+                cartItems: [
+                  ...state.cartItems.filter((item) => item.productId !== id),
+                ],
+              };
+            }
+
+            return {
+              cartItems: [
+                ...state.cartItems.filter((item) => item.productId !== id),
+                { productId: id, amount },
+              ],
+            };
+          }
+
+          throw new Error("Not found.");
+        });
+      },
+    }),
+    { name: "cart-storage" },
+  ),
+);
