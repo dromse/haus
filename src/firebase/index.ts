22086241e0fe6dcd91ddf629a63f52d878e0abfb
dromse <dromasko4@gmail.com>
2024-05-04 21:45:43 +0300

    expose only apikey for firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,14 +1,7 @@
-import { FirebaseOptions, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
 };
 
-console.log(firebaseConfig)
 export const firebaseApp = initializeApp(firebaseConfig);
